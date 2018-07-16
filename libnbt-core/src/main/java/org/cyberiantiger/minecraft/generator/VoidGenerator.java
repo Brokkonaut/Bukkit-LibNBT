2917eaac00baab3e2a8db502c5aa9ffc1d523e4b
Brokkonaut <hannos17@gmx.de>
2018-07-16 22:43:56 +0200

    Update VoidGenerator to 1.13

--- libnbt-core/src/main/java/org/cyberiantiger/minecraft/generator/VoidGenerator.java
+++ libnbt-core/src/main/java/org/cyberiantiger/minecraft/generator/VoidGenerator.java
@@ -4,10 +4,14 @@
  */
 package org.cyberiantiger.minecraft.generator;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
+import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
 import org.cyberiantiger.minecraft.Coord;
 
@@ -33,13 +37,9 @@ public class VoidGenerator extends ChunkGenerator {
         this.biome = biome;
     }
 
-    @Override
     public byte[][] generateBlockSections(World world, Random random, int x, int z, BiomeGrid biomes) {
         int maxHeight = world.getMaxHeight();
         byte[][] chunk = new byte[maxHeight >> 4][];
-
-        int xPos = x * 16;
-        int zPos = z * 16;
 
         for (int i = 0; i < 16; i++) {
             for (int j = 0; j < 16; j++) {
@@ -47,6 +47,20 @@ public class VoidGenerator extends ChunkGenerator {
             }
         }
         return chunk;
+    }
+
+    public ChunkData generateChunkData(World world, Random random, int x, int z, BiomeGrid biome) {
+        ChunkData cd = createChunkData(world);
+        for (int xx = 0; xx < 16; xx++) {
+            for (int zz = 0; zz < 16; zz++) {
+                biome.setBiome(xx, zz, Biome.PLAINS);
+            }
+        }
+        return cd;
+    }
+
+    public List<BlockPopulator> getDefaultPopulators(World world) {
+        return Collections.emptyList();
     }
 
     @Override
@@ -58,6 +72,5 @@ public class VoidGenerator extends ChunkGenerator {
     public boolean canSpawn(World world, int x, int z) {
         return super.canSpawn(world, x, z);
     }
-
 
 }
